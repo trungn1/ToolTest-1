2f6a863b021d8f265b8cec3e65db186b72eb45c4
trung <trung@watchtower.ai>
2020-02-28 14:23:58 -0800

    change password

--- password-test.py
+++ password-test.py
@@ -3,8 +3,7 @@ import pymongo
 MONGO_HOST = "10.1.0.90"
 MONGO_PORT = 25
 MONGO_DB = "db_name"
-MONGO_USER = "username"
-MONGO_PASS = "fee3fa69-d0db-4efe-8371-3d24d65cb426"
+MONGO_USER = "INPUT_PASSWORD_HERE"
 
 con = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
 db = con[MONGO_DB]
